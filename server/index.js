736d30af267791d0bcaa22ca9d1f695f26f3a3f4
Ayush-Petwal <ayushpetwal003@gmail.com>
2025-03-09 21:37:59 +0530

    some changes

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(cors({
     origin:process.env.FRONTEND_URL,
     credentials:true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'] 
+    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
  
 // apis
